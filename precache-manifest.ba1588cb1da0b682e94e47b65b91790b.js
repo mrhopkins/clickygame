0bafaf74154123218edbb5f70b98baf7117df20a
Matthew Hopkins <mattrhopkins@gmail.com>
2019-04-28 23:25:32 -0600

    Updates

---
+++ precache-manifest.ba1588cb1da0b682e94e47b65b91790b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "781bcf0a1105cd353461b6f37b5b0673",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "b497796679261a02ede6",
+    "url": "/clickygame/static/css/main.6fe08b32.chunk.css"
+  },
+  {
+    "revision": "015be0247dc3ee2955cc",
+    "url": "/clickygame/static/js/2.76d8ed81.chunk.js"
+  },
+  {
+    "revision": "b497796679261a02ede6",
+    "url": "/clickygame/static/js/main.80966121.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "9a30e6c6c75344e5bd217bbe9720ef3e",
+    "url": "/clickygame/static/media/bg.9a30e6c6.png"
+  },
+  {
+    "revision": "ac08e281fd95deb21705e5d4571baacd",
+    "url": "/clickygame/static/media/candyman.ac08e281.jpg"
+  },
+  {
+    "revision": "1c8258c21d54e0e93248ec499718bb2a",
+    "url": "/clickygame/static/media/chucky.1c8258c2.jpg"
+  },
+  {
+    "revision": "b1b66dde667e39a079bd33f1facf6049",
+    "url": "/clickygame/static/media/damien.b1b66dde.jpg"
+  },
+  {
+    "revision": "7bb361da394807de75377bc475964ead",
+    "url": "/clickygame/static/media/freddy.7bb361da.jpg"
+  },
+  {
+    "revision": "b83cad3b3c40a19d0ff070c88f61d834",
+    "url": "/clickygame/static/media/ghostface.b83cad3b.jpg"
+  },
+  {
+    "revision": "6dea1f652f320df01ee3b9d555cdcc35",
+    "url": "/clickygame/static/media/hannibal.6dea1f65.jpg"
+  },
+  {
+    "revision": "fee013410dd9d378dfb30256e3532e39",
+    "url": "/clickygame/static/media/jason.fee01341.jpg"
+  },
+  {
+    "revision": "81001856359d0dae92e1049c539613c1",
+    "url": "/clickygame/static/media/leatherface.81001856.jpg"
+  },
+  {
+    "revision": "26fe2fde6bd47de966590341182df49e",
+    "url": "/clickygame/static/media/michael.26fe2fde.jpg"
+  },
+  {
+    "revision": "a6cb41999563cc295dc8b31e33b11e23",
+    "url": "/clickygame/static/media/pennywise.a6cb4199.png"
+  },
+  {
+    "revision": "1d3cece2a6da494bdaf6477dcd6db029",
+    "url": "/clickygame/static/media/pinhead.1d3cece2.jpg"
+  },
+  {
+    "revision": "2c3ae4467a381cf608d4a783ebcaa2ad",
+    "url": "/clickygame/static/media/regan.2c3ae446.jpg"
+  }
+]);
